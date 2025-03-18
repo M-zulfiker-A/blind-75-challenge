48c623332a3abf4e103d6a1deed17c6d4b6b73dd
MUHAMMED ZULFIKER A <54462409+M-zulfiker-A@users.noreply.github.com>
2025-03-18 06:59:36 +0530

    add solution to two-sum in typescript

---
+++ Challenge-1/two-sum.ts
@@ -0,0 +1,13 @@
+function twoSum(nums: number[], target: number): number[] {
+  const map: Map<number, number> = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement)!, i];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+}
+
+twoSum([2, 7, 11, 15], 9); // [0, 1]
